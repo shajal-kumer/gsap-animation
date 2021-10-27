bcbf63df4397434f9d185b15ac02b0b19032551f
shajal-kumer <shajalkumer@gmail.com>
2021-10-27 08:06:18 +0600

    distortion hover effect

---
+++ distortion-hover-effect/app.js
@@ -0,0 +1,8 @@
+new hoverEffect({
+	parent: document.querySelector(".distortion"),
+	intensity: 0.1,
+	angle: Math.PI / 8,
+	image1: "./bg-1.jpg",
+	image2: "./bg-2.jpg",
+	displacementImage: "./heightMap.png",
+});
